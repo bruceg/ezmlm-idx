4edd0b065f76322bd259463155b04a21ae419790
Bruce Guenter <bruce@untroubled.org>
2005-02-09 21:40:33 +0000

    Added another missing source from the previous merge.

---
+++ hdr_listsubject.c
@@ -0,0 +1,24 @@
+/*$Id$*/
+#include "hdr.h"
+#include "qmail.h"
+#include "stralloc.h"
+#include "quote.h"
+
+extern struct qmail qq;
+extern void die_nomem(void);
+extern stralloc quoted;
+extern stralloc outlocal;
+extern stralloc outhost;
+
+void hdr_listsubject3(const char *a,const char *b,const char *c)
+{
+  if (!quote(&quoted,&outlocal)) die_nomem();
+  qmail_puts(&qq,"Subject: ");
+  if (a != 0) qmail_puts(&qq,a);
+  if (b != 0) qmail_puts(&qq,b);
+  if (c != 0) qmail_puts(&qq,c);
+  qmail_put(&qq,quoted.s,quoted.len);
+  qmail_puts(&qq,"@");
+  qmail_put(&qq,outhost.s,outhost.len);
+  qmail_puts(&qq,"\n");
+}
