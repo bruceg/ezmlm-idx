242b1c27096757c3d2d28ad12bf3b0258a68a95a
Bruce Guenter <bruce@untroubled.org>
2005-04-15 17:43:08 +0000

    Reference the ezmlm sources within the repository.

--- makepatch.sh
+++ makepatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # $Id$
 trap 'rm -r ezmlm-0.53' EXIT
-tar -xzf ../ezmlm-0.53.tar.gz
-diff -u -X REPLACED -x '*.[ao]' -x '*~' -x '*.do' ezmlm-0.53 . \
+tar -xzf ezmlm-0.53.tar.gz
+diff -u -X REPLACED -x '*.[ao]' -x '*~' -x '*.do' -x '*=*' ezmlm-0.53 . \
 | sed	\
 	-e "s/^--- ezmlm-0.53\\//--- /" \
 	-e "s/^+++ .\\//+++ /" \
