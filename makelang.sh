8e2fce2aea1d1c721778fddcca33733dfd8609ab
Bruce Guenter <bruce@untroubled.org>
2005-09-30 20:51:42 +0000

    Build the language-specific ezmlmrc directly into the lang directory, and skip adding any additional files (ie mailinglist or charset).

--- makelang.sh
+++ makelang.sh
@@ -4,15 +4,12 @@ test x"$1" = x && {
   exit 1
 }
 
-test -f lang/"$1".add -a -f lang/"$1".sed || {
+test -d lang/"$1" -a -f lang/"$1"/sed || {
   echo "$0: language \"$1\" does not appear to exist."
   exit 1
 }
 
 (
   echo `sed -e 's/^.*-//' -e q VERSION` - This version identifier must be on line 1 and start in pos 1.
-  sed -f lang/"$1".sed ezmlmrc.template
-  cat lang/"$1".add
-  echo "</language#E/>"
-  echo "$1"
-) > ezmlmrc."$1"
+  sed -f lang/"$1"/sed ezmlmrc.template
+) > lang/$1/ezmlmrc
