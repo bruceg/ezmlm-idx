961c3491ed3e1534a3c9ea2ba35507f3ed9f09f7
Bruce Guenter <bruce@untroubled.org>
2005-10-02 04:31:23 +0000

    Add an external "struct qmail" declaration in copy.h to avoid a warning.

--- copy.h
+++ copy.h
@@ -1,6 +1,8 @@
 #ifndef COPY_H
 #define COPY_H
 
+struct qmail;
+
 extern void copy_xlate(stralloc *out,const stralloc *in,char q);
 extern void copy(struct qmail *qq,const char *fn,char q);
 extern void set_cptarget(const char *);
