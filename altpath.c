4d57d4232391cd28f241d4572f004dade71e9708
Bruce Guenter <bruce@untroubled.org>
2005-10-04 19:23:18 +0000

    Pull files from the default language (`conf-etc`/default) if they cannot be located in either the list directory or the ezmlmrc directory.

--- altpath.c
+++ altpath.c
@@ -7,6 +7,8 @@
 #include "open.h"
 #include "slurp.h"
 #include "altpath.h"
+#include "idx.h"
+#include "auto_etc.h"
 
 static stralloc path = {0};
 
@@ -19,26 +21,38 @@ const char *altpath(stralloc *s,const char *fn)
   return s->s;
 }
 
+const char *altdefaultpath(stralloc *s,const char *fn)
+{
+  if (!stralloc_copys(s,auto_etc)) die_nomem();
+  if (!stralloc_cats(s,TXT_DEFAULT)) die_nomem();
+  if (!stralloc_append(s,"/")) die_nomem();
+  if (!stralloc_cats(s,fn)) die_nomem();
+  if (!stralloc_0(s)) die_nomem();
+  return s->s;
+}
+
 int alt_open_read(const char *fn)
 {
   int fd;
   if ((fd = open_read(fn)) == -1
-      && errno == error_noent
-      && ezmlmrc.len > 0)
-    fd = open_read(altpath(&path,fn));
+      && errno == error_noent) {
+    if (ezmlmrc.len > 0)
+      fd = open_read(altpath(&path,fn));
+    if (fd == -1
+	&& errno == error_noent)
+      fd = open_read(altdefaultpath(&path,fn));
+  }
   return fd;
 }
 
 int alt_slurp(const char *fn,struct stralloc *sa,int bufsize)
 {
-  switch (slurp(fn,sa,bufsize)) {
-  case -1:
-    return -1;
-  case 0:
-    if (ezmlmrc.len != 0)
-      return slurp(altpath(&path,fn),sa,bufsize);
-    return 0;
-  default:
-    return 1;
+  int r;
+  if ((r = slurp(fn,sa,bufsize)) == 0) {
+    if (ezmlmrc.len > 0)
+      r = slurp(altpath(&path,fn),sa,bufsize);
+    if (r == 0)
+      r = slurp(altdefaultpath(&path,fn),sa,bufsize);
   }
+  return r;
 }
