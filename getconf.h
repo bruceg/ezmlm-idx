d31a142219956805e3b4945a2bf9a1fdde01485c
Bruce Guenter <bruce@untroubled.org>
2007-11-11 22:51:56 -0600

    Forward declare struct stralloc in getconf.h

--- getconf.h
+++ getconf.h
@@ -1,9 +1,10 @@
 #ifndef GETCONF_H
 #define GETCONF_H
 
-extern int getconf(stralloc *sa,const char *fn,int flagrequired);
+struct stralloc;
+extern int getconf(struct stralloc *sa,const char *fn,int flagrequired);
 extern int getconf_isset(const char *fn);
-extern int getconf_line(stralloc *sa,const char *fn,int flagrequired);
+extern int getconf_line(struct stralloc *sa,const char *fn,int flagrequired);
 extern int getconf_ulong(unsigned long *n,const char *fn,int flagrequired);
 extern int getconf_ulong2(unsigned long *n0,unsigned long *n1,
 			  const char *fn,int flagrequired);
