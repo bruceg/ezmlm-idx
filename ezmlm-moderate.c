bruceg/ezmlm-idx

commit fa590071f82a3609252731aa7039ad0d0962e9fa

Allow execution of argument lists in ezmlm-confirm/moderate.
The previous code for ezmlm-confirm and ezmlm-moderate would execute the
optional program parameter as a single string using /bin/sh.  This patch
allows for execution using execvp simply.  This reduces the overhead and
eliminates any possibility of the shell parsing the arguments wrong.

ezmlm-moderate.c
@@ -434,7 +434,9 @@ void main(int argc,char **argv)
     if ((child = wrap_fork()) == 0) {
       close(0);
       dup(fd);	/* make fnmsg.s stdin */
-      if (argc > optind)
+      if (argc > optind + 1)
+	wrap_execvp((const char **)argv + optind);
+      else if (argc > optind)
         wrap_execsh(argv[optind]);
       else
         wrap_execbin("/ezmlm-send", &sendopt, dir);

ezmlm-confirm.1
@@ -4,7 +4,7 @@ ezmlm-confirm \- process user actions for message
 confirm and discard
 .SH SYNOPSIS
 .B ezmlm-confirm [-cCrRvV]
-.I dir ['/path/program args']
+.I dir [ /path/program args ]
 
 .SH DESCRIPTION
 .B ezmlm-confirm
@@ -34,8 +34,8 @@ as the first argument.
 
 If the optional second argument is given,
 .B ezmlm-confirm
-pipes the message to that program, as executed by sh.
-No parameters are supplied.
+pipes the message to that program.
+No additional parameters are supplied.
 
 .I dir
 is passed as an argument to

ezmlm-confirm.c
@@ -202,7 +202,9 @@ void main(int argc, char **argv)
     if ((child = wrap_fork()) == 0) {
       close(0);
       dup(fd);	/* make fnmsg.s stdin */
-      if (argc > optind)
+      if (argc > optind + 1)
+	wrap_execvp((const char **)argv + optind);
+      else if (argc > optind)
         wrap_execsh(argv[optind]);
       else
         wrap_execbin("/ezmlm-send", &sendopt, dir);

ezmlm-moderate.1
@@ -5,7 +5,7 @@ acceptance and rejection
 .SH SYNOPSIS
 .B ezmlm-moderate [-cCmMrRvV]
 .B [-t\fI reply-to@host\fB]
-.I dir ['/path/program args']
+.I dir [ /path/program args ... ]
 
 .SH DESCRIPTION
 .B ezmlm-moderate
@@ -35,8 +35,8 @@ as the first argument.
 
 If the optional second argument is given,
 .B ezmlm-moderate
-pipes the message to that program, as executed by sh.
-No parameters are supplied.
+pipes the message to that program.
+No additional parameters are supplied.
 
 .I dir
 is passed as an argument to

ezmlmrc.template
@@ -199,13 +199,13 @@
 </confirmer#y/>
 <#B#>/ezmlm-weed
 </#MUy/>
-|<#B#>/ezmlm-confirm '<#D#>' "<#B#>/ezmlm-store -Y '<#D#>'"
+|<#B#>/ezmlm-confirm '<#D#>' <#B#>/ezmlm-store -Y '<#D#>'
 </#Muy/>
-|<#B#>/ezmlm-confirm '<#D#>' "<#B#>/ezmlm-store -Y '<#D#>'"
+|<#B#>/ezmlm-confirm '<#D#>' <#B#>/ezmlm-store -Y '<#D#>'
 </#mUy/>
-|<#B#>/ezmlm-confirm '<#D#>' "<#B#>/ezmlm-store -Y '<#D#>'"
+|<#B#>/ezmlm-confirm '<#D#>' <#B#>/ezmlm-store -Y '<#D#>'
 </#muy/>
-|<#B#>/ezmlm-confirm '<#D#>' "<#B#>/ezmlm-gate -Y '<#D#>' . digest allow mod"
+|<#B#>/ezmlm-confirm '<#D#>' <#B#>/ezmlm-gate -Y '<#D#>' . digest allow mod
 </#iy/>
 |<#B#>/ezmlm-archive '<#D#>' || exit 0
 </#y/>

tests/270-ezmlm-moderate
@@ -34,7 +34,7 @@ qqclean
 # ACC is set in 145-ezmlm-store, as the accept address for "mod3" message
 LOCAL=`${ECHO} "$ACC" | cut -d@ -f1`
 DEFAULT=`${ECHO} "$LOCAL" | cut -c"$ACCLEN"-`
-${EZBIN}/ezmlm-moderate "$DIR" "${EZBIN}/ezmlm-send ${DIR}" \
+${EZBIN}/ezmlm-moderate "$DIR" ${EZBIN}/ezmlm-send "$DIR" \
 	 </dev/null >"$ERR" 2>&1 || \
 		fatal "failed on acceptance"
 msgexists || \

commit 7854cf22b06c9869595d60dcbc0355426aff376d

Fixed unitialized dir in ezmlm-moderate.c.
This bug was introduced in previous change
fa590071f82a3609252731aa7039ad0d0962e9fa titled "Allow execution of
argument lists in ezmlm-confirm/moderate".

ezmlm-moderate.c
@@ -80,7 +80,7 @@ stralloc fnsub = {0};
 char subbuf[256];
 substdio sssub;
 
-const char *dir;
+static const char *dir;
 
 struct stat st;
 
@@ -187,7 +187,7 @@ void main(int argc,char **argv)
   when = now();
 
   if (!stralloc_copys(&sendopt,"-")) die_nomem();
-  opt = getconfopt(argc,argv,options,1,0);
+  opt = getconfopt(argc,argv,options,1,&dir);
 
   sender = get_sender();
   if (!sender) strerr_die2x(100,FATAL,MSG(ERR_NOSENDER));