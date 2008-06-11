bruceg/ezmlm-idx

commit b49e698c2c7ea2560eda6e1319c7f426783299db

Created a new "wrap" library, used for merging common code trapping error exits from system calls.

ezmlm-store.c
@@ -31,6 +31,7 @@
 #include "copy.h"
 #include "subscribe.h"
 #include "mime.h"
+#include "wrap.h"
 
 int flagmime = MOD_MIME;	/* default is message as attachment */
 int flagpublic = 1;		/* default anyone can post */
@@ -161,8 +162,8 @@ char **argv;
   int opt;
   unsigned int i;
   char szchar[2] = "-";
-  char *sendargs[4];
-  int child,wstat;
+  const char *sendargs[4];
+  int child;
 
   (void) umask(022);
   sig_pipeignore();
@@ -223,27 +224,10 @@ char **argv;
       case -1:
         strerr_die2sys(111,FATAL,ERR_FORK);
       case 0:
-        execvp(*sendargs,sendargs);
-        if (errno == error_txtbsy || errno == error_nomem ||
-            errno == error_io)
-          strerr_die5sys(111,FATAL,ERR_EXECUTE,"/bin/sh -c ",sendargs[2],": ");
-        else
-          strerr_die5sys(100,FATAL,ERR_EXECUTE,"/bin/sh -c ",sendargs[2],": ");
+	wrap_execvp(sendargs, FATAL);
     }
          /* parent */
-    wait_pid(&wstat,child);
-    if (wait_crashed(wstat))
-      strerr_die2x(111,FATAL,ERR_CHILD_CRASHED);
-    switch(wait_exitcode(wstat)) {
-      case 100:
-        strerr_die2x(100,FATAL,"Fatal error from child");
-      case 111:
-         strerr_die2x(111,FATAL,"Temporary error from child");
-      case 0:
-        _exit(0);
-      default:
-        strerr_die2x(111,FATAL,"Unknown temporary error from child");
-    }
+    wrap_exitcode(child, FATAL);
   }
 
   if (!moderators.len || !(moderators.s[0] == '/')) {

Makefile
@@ -336,8 +336,8 @@ date822fmt.h direntry.h fmt.h strerr.h errtxt.h idx.h sgetopt.h subgetopt.h
 ezmlm-cron: \
 load ezmlm-cron.o strerr.a stralloc.a alloc.a error.a open.a auto_qmail.o \
 getopt.a getln.a str.a substdio.a sig.a fs.a open.a fd.a lock.a wait.a \
-case.a auto_cron.o
-	./load ezmlm-cron getopt.a getln.a strerr.a substdio.a \
+case.a auto_cron.o wrap.a
+	./load ezmlm-cron getopt.a getln.a wrap.a strerr.a substdio.a \
 	stralloc.a alloc.a sig.a fs.a open.a fd.a lock.a error.a \
 	wait.a case.a str.a auto_qmail.o auto_cron.o
 
@@ -354,9 +354,9 @@ fmt.h auto_cron.h
 ezmlm-gate: \
 load ezmlm-gate.o subdb.a auto_bin.o getopt.a getln.a env.a sig.a strerr.a \
 stralloc.a alloc.a error.a str.a case.a wait.a substdio.a open.a lock.a \
-fs.a getconf.o slurpclose.o slurp.o seek.a sql.lib
+fs.a getconf.o slurpclose.o slurp.o seek.a wrap.a sql.lib
 	./load ezmlm-gate subdb.a getconf.o slurpclose.o slurp.o \
-	getopt.a getln.a auto_bin.o env.a sig.a fs.a \
+	getopt.a getln.a auto_bin.o env.a sig.a fs.a wrap.a \
 	strerr.a substdio.a stralloc.a alloc.a error.a str.a case.a wait.a \
 	open.a lock.a seek.a `cat sql.lib`
 
@@ -578,11 +578,11 @@ ezmlm-mktab.1
 ezmlm-moderate: \
 load ezmlm-moderate.o auto_qmail.o getconf.o auto_bin.o copy.o mime.a \
 cookie.o now.o datetime.o date822fmt.o slurpclose.o slurp.o qmail.o quote.o \
-surf.a getln.a env.a sig.a strerr.a substdio.a stralloc.a alloc.a \
+surf.a getln.a env.a sig.a strerr.a substdio.a stralloc.a alloc.a wrap.a \
 error.a str.a fs.a case.a open.a seek.a wait.a lock.a fd.a getopt.a
 	./load ezmlm-moderate auto_qmail.o getconf.o copy.o mime.a \
-	cookie.o now.o datetime.o date822fmt.o slurpclose.o \
-	slurp.o qmail.o quote.o surf.a getln.a env.a sig.a strerr.a \
+	cookie.o now.o datetime.o date822fmt.o slurpclose.o slurp.o \
+	qmail.o quote.o surf.a getln.a env.a sig.a wrap.a strerr.a \
 	substdio.a stralloc.a alloc.a error.a str.a fs.a case.a \
 	auto_bin.o open.a seek.a wait.a lock.a fd.a getopt.a
 
@@ -757,9 +757,10 @@ ezmlm-store: \
 load ezmlm-store.o auto_qmail.o getconf.o subdb.a log.o auto_bin.o mime.a \
 cookie.o now.o datetime.o date822fmt.o slurpclose.o slurp.o qmail.o quote.o \
 surf.a getln.a env.a sig.a strerr.a substdio.a stralloc.a alloc.a sql.lib \
-error.a str.a fs.a case.a open.a seek.a wait.a lock.a fd.a getopt.a copy.o
+error.a str.a fs.a case.a open.a seek.a wait.a lock.a fd.a getopt.a copy.o \
+wrap.a
 	./load ezmlm-store auto_qmail.o getconf.o subdb.a copy.o mime.a \
-	log.o cookie.o now.o datetime.o date822fmt.o slurpclose.o \
+	log.o cookie.o now.o datetime.o date822fmt.o slurpclose.o wrap.a \
 	slurp.o qmail.o quote.o surf.a getln.a env.a sig.a strerr.a \
 	substdio.a stralloc.a alloc.a error.a str.a fs.a case.a \
 	open.a seek.a wait.a lock.a fd.a getopt.a auto_bin.o `cat sql.lib`
@@ -1563,6 +1564,22 @@ makelib wait_pid.o
 wait_pid.o: \
 compile wait_pid.c wait_pid.c wait_pid.c error.h haswaitp.h
 	./compile wait_pid.c
+
+wrap.a: \
+makelib wrap_execv.o wrap_exitcode.o wrap_waitpid.o
+	./makelib wrap.a wrap_execv.o wrap_exitcode.o wrap_waitpid.o
+
+wrap_execv.o: \
+compile wrap_execv.c error.h errtxt.h strerr.h wrap.h
+	./compile wrap_execv.c
+
+wrap_exitcode.o: \
+compile wrap_exitcode.c errtxt.h strerr.h wrap.h
+	./compile wrap_exitcode.c
+
+wrap_waitpid.o: \
+compile wrap_waitpid.c wait.h errtxt.h strerr.h wrap.h
+	./compile wrap_waitpid.c
 
 yyyymm.a: \
 makelib date2yyyymm.o dateline.o

ezmlm-cron.c
@@ -14,6 +14,7 @@
 #include "auto_cron.h"
 #include "errtxt.h"
 #include "idx.h"
+#include "wrap.h"
 
 #define FATAL "ezmlm-cron: fatal: "
 
@@ -129,8 +130,7 @@ char **argv;
 
 {
   int child;
-  char *sendargs[4];
-  int wstat;
+  const char *sendargs[4];
 
   (void) umask(077);
   sig_pipeignore();
@@ -487,18 +487,10 @@ char **argv;
       case 0:
         if (setreuid(euid,euid) == -1)
           strerr_die2sys(100,FATAL,ERR_SETUID);
-        execvp(*sendargs,sendargs);
-        if (errno == error_txtbsy || errno == error_nomem ||
-            errno == error_io)
-          strerr_die4sys(111,FATAL,ERR_EXECUTE,sendargs[2],": ");
-        else
-          strerr_die4sys(100,FATAL,ERR_EXECUTE,sendargs[2],": ");
+	wrap_execvp(sendargs, FATAL);
   }
          /* parent */
-  wait_pid(&wstat,child);
-  if (wait_crashed(wstat))
-    strerr_die2x(111,FATAL,ERR_CHILD_CRASHED);
-  switch(wait_exitcode(wstat)) {
+  switch (wrap_waitpid(child, FATAL)) {
       case 0:
         _exit(0);
       default:

ezmlm-gate.c
@@ -18,6 +18,7 @@
 #include "errtxt.h"
 #include "idx.h"
 #include "subscribe.h"
+#include "wrap.h"
 
 #define FATAL "ezmlm-gate: fatal: "
 
@@ -36,9 +37,8 @@ stralloc storeopt = {0};
 void *psql = (void *) 0;
 
 char szchar[2] = "-";
-  char *sendargs[4];
+  const char *sendargs[4];
   int child;
-  int wstat;
   const char *pmod;
 
 int mailprog(s)
@@ -54,20 +54,10 @@ int mailprog(s)
       case -1:
 	strerr_die2sys(111,FATAL,ERR_FORK);
       case 0:
-	execv(*sendargs,sendargs);
-	if (errno == error_txtbsy || errno == error_nomem ||
-	  errno == error_io)
-	        strerr_die5sys(111,FATAL,ERR_EXECUTE,
-		  "/bin/sh -c ",sendargs[2],": ");
-        else
-		strerr_die5sys(100,FATAL,ERR_EXECUTE,
-		  "/bin/sh -c ",sendargs[2],": ");
+	wrap_execv(sendargs, FATAL);
     }
          /* parent */
-    wait_pid(&wstat,child);
-    if (wait_crashed(wstat))
-	strerr_die2x(111,FATAL,ERR_CHILD_CRASHED);
-    switch((r = wait_exitcode(wstat))) {
+    switch((r = wrap_waitpid(child, FATAL))) {
       case 0: case 99: case 100: break;
       case 111:					/* temp error */
         strerr_die2x(111,FATAL,ERR_CHILD_TEMP);
@@ -188,7 +178,7 @@ char **argv;
   sendargs[3] = 0;
 
   if (dontact) {
-    char **s;
+    const char **s;
     for (s = sendargs; *s; s++) {
       substdio_puts(subfderr, *s);
       if (s[1]) substdio_puts(subfderr, " ");
@@ -201,26 +191,9 @@ char **argv;
     case -1:
       strerr_die2sys(111,FATAL,ERR_FORK);
     case 0:
-      execv(*sendargs,sendargs);
-      if (errno == error_txtbsy || errno == error_nomem ||
-          errno == error_io)
-        strerr_die5sys(111,FATAL,ERR_EXECUTE,"/bin/sh -c ",sendargs[2],": ");
-      else
-        strerr_die5sys(100,FATAL,ERR_EXECUTE,"/bin/sh -c ",sendargs[2],": ");
+      wrap_execv(sendargs, FATAL);
    }
          /* parent */
-   wait_pid(&wstat,child);
-   if (wait_crashed(wstat))
-     strerr_die2x(111,FATAL,ERR_CHILD_CRASHED);
-   switch(wait_exitcode(wstat)) {
-     case 100:
-       strerr_die2x(100,FATAL,ERR_CHILD_FATAL);
-     case 111:
-        strerr_die2x(111,FATAL,ERR_CHILD_TEMP);
-     case 0:
-       _exit(0);
-     default:
-       strerr_die2x(111,FATAL,ERR_CHILD_UNKNOWN);
-   }
+   wrap_exitcode(child, FATAL);
 }
 

ezmlm-moderate.c
@@ -31,6 +31,7 @@
 #include "errtxt.h"
 #include "copy.h"
 #include "idx.h"
+#include "wrap.h"
 
 int flagmime = MOD_MIME;	/* default is message as attachment */
 char flagcd = '\0';		/* default: do not use transfer encoding */
@@ -211,9 +212,8 @@ char **argv;
   unsigned int pos,i;
   int child;
   int opt;
-  char *sendargs[4];
+  const char *sendargs[4];
   char *cp,*cpnext,*cpfirst,*cplast,*cpafter;
-  int wstat;
 
   (void) umask(022);
   sig_pipeignore();
@@ -574,28 +574,11 @@ char **argv;
       case 0:		/* child */
         close(0);
         dup(fd);	/* make fnmsg.s stdin */
-        execv(*sendargs,sendargs);
-        if (errno == error_txtbsy || errno == error_nomem ||
-            errno == error_io)
-          strerr_die5sys(111,FATAL,ERR_EXECUTE,"/bin/sh -c ",sendargs[2],": ");
-        else
-          strerr_die5sys(100,FATAL,ERR_EXECUTE,"/bin/sh -c ",sendargs[2],": ");
-       }
+	wrap_execv(sendargs, FATAL);
+    }
          /* parent */
-      wait_pid(&wstat,child);
       close(fd);
-      if (wait_crashed(wstat))
-        strerr_die3x(111,FATAL,sendargs[2],ERR_CHILD_CRASHED);
-      switch(wait_exitcode(wstat)) {
-        case 100:
-          strerr_die2x(100,FATAL,"Fatal error from child");
-        case 111:
-           strerr_die2x(111,FATAL,"Temporary error from child");
-        case 0:
-          break;
-        default:
-          strerr_die2x(111,FATAL,"Unknown temporary error from child");
-      }
+      wrap_exitcode(child, FATAL);
       if (!stralloc_copys(&fnnew,"mod/accepted/")) die_nomem();
 
       if (!stralloc_cats(&fnnew,fnbase.s)) die_nomem();

wrap.h
@@ -1,0 +1,9 @@
+#ifndef WRAP_H
+#define WRAP_H
+
+extern void wrap_execv(const char **argv, const char *FATAL);
+extern void wrap_execvp(const char **argv, const char *FATAL);
+extern void wrap_exitcode(int pid, const char *FATAL);
+extern int wrap_waitpid(int pid, const char *FATAL);
+
+#endif

wrap_execv.c
@@ -1,0 +1,27 @@
+#include "error.h"
+#include "errtxt.h"
+#include "strerr.h"
+#include "wrap.h"
+
+static void wrapper(int (*fn)(char*,char**),
+		    const char **args, const char *FATAL)
+{
+  fn((char*)*args, (char**)args);
+  strerr_die4sys((errno == error_txtbsy
+                 || errno == error_nomem
+		 || errno == error_io
+		 ) ? 111 : 100, FATAL, ERR_EXECUTE, args[0], ": ");
+}
+
+extern int execv(char*,char**);
+extern int execvp(char*,char**);
+
+void wrap_execv(const char **args, const char *FATAL)
+{
+  wrapper(execv, args, FATAL);
+}
+
+void wrap_execvp(const char **args, const char *FATAL)
+{
+  wrapper(execvp, args, FATAL);
+}

wrap_exitcode.c
@@ -1,0 +1,17 @@
+#include "errtxt.h"
+#include "strerr.h"
+#include "wrap.h"
+
+void wrap_exitcode(int pid, const char *FATAL)
+{
+  switch (wrap_waitpid(pid, FATAL)) {
+    case 100:
+      strerr_die2x(100,FATAL,ERR_CHILD_FATAL);
+    case 111:
+      strerr_die2x(111,FATAL,ERR_CHILD_TEMP);
+    case 0:
+      break;
+    default:
+      strerr_die2x(111,FATAL,ERR_CHILD_UNKNOWN);
+  }
+}

commit 4b1d029d5fdca18d3bbebb25d2ba00f567edfea9

Fixed ezmlm-store to exec ezmlm-send when both !modpost and !confirmpost.
This bug was introduced in b49e698c2c7ea2560eda6e1319c7f426783299db.
Prior to this, ezmlm-store would exit after spawning ezmlm-send.  Doing
a fork, exec, and then unconditionally exiting is a waste of resources,
so just exec instead when neither modpost or confirmpost are set.

ezmlm-store.c
@@ -146,7 +146,6 @@ void main(int argc,char **argv)
   stralloc mod = {0};
   const char *err;
   unsigned int i;
-  int child;
 
   (void) umask(022);
   sig_pipeignore();
@@ -164,12 +163,8 @@ void main(int argc,char **argv)
 
   flagmodpost = getconf_line(&moderators,"modpost",0);
   flagremote = getconf_isset("remote");
-  if (!flagmodpost && !flagconfirm) {	/* not msg-mod. Pipe to ezmlm-send */
-    if ((child = wrap_fork()) == 0)
-      wrap_execbin("/ezmlm-send", &sendopt, dir);
-    /* parent */
-    wrap_exitcode(child);
-  }
+  if (!flagmodpost && !flagconfirm) /* not msg-mod. Exec ezmlm-send */
+    wrap_execbin("/ezmlm-send", &sendopt, dir);
 
   if (!moderators.len) {
     if (!stralloc_copys(&moderators,"mod")) die_nomem();

tests/145-ezmlm-store
@@ -6,6 +6,7 @@
 
   SENDER="${SND}@$HOST"
   ${EZBIN}/ezmlm-sub "$DIR" mod "$SENDER"
+  ${EZBIN}/ezmlm-sub "$DIR" "subscriber@$HOST"
 
 # message from mod, normal use -> should queue
   qqclean
@@ -36,7 +37,7 @@
 	    EZFORK='no'
 	  fi
 	}
-  checkenv mod2 "$LOC-return-@$HOST" "$SENDER"
+  checkenv mod2 "$LOC-return-.*@$HOST-@\\[\\]" "subscriber@$HOST"
 
   cat ${DIR}/mod/pending/* | ${GREP} "mod2" > /dev/null && \
 	fatal "queued message 2 despite non-modpost"
@@ -92,5 +93,7 @@
 
   test -s "$QQMSG" && \
 	fatal "ezmlm-store: modpostonly failed to reject message from non-mod"
+
+  ${EZBIN}/ezmlm-unsub "$DIR" "subscriber@$HOST"
 
   ${ECHO} "OK"