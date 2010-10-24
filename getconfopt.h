ef0698bf87301121b7ebae1a741173b6a0d0a196
Bruce Guenter <bruce@untroubled.org>
2010-10-24 16:25:17 -0600

    getconfopt: Remove unused "counter" option type

--- getconfopt.h
+++ getconfopt.h
@@ -12,7 +12,6 @@ struct option_type
 };
 
 extern const struct option_type opt_copy_flag;
-extern const struct option_type opt_counter;
 extern const struct option_type opt_cstr;
 extern const struct option_type opt_cstr_flag;
 extern const struct option_type opt_flag;
@@ -43,7 +42,6 @@ struct option
 };
 
 #define OPT_COPY_FLAG(VAR,CH) {{.str=&VAR},CH,&opt_copy_flag,{0},0,0}
-#define OPT_COUNTER(VAR,CH,VALUE,FILENAME) {{.flag=&VAR},CH,&opt_counter,{.flag=VALUE},FILENAME,0}
 #define OPT_CSTR(VAR,CH,FILENAME) {{.cstr=&VAR},CH,&opt_cstr,{0},FILENAME,0}
 #define OPT_CSTR_FLAG(VAR,CH,VALUE,FILENAME) {{.cstr=&VAR},CH,&opt_cstr_flag,{.cstr=VALUE},FILENAME,0}
 #define OPT_FLAG(VAR,CH,VALUE,FILENAME) {{.flag=&VAR},CH,&opt_flag,{.flag=VALUE},FILENAME,0}
--- getconfopt_flag.c
+++ getconfopt_flag.c
@@ -8,13 +8,6 @@ static int parse(struct option *o,const char *arg)
   (void)arg;
 }
 
-static int counter(struct option *o,const char *arg)
-{
-  *o->var.flag += o->value.flag;
-  return 1;
-  (void)arg;
-}
-
 static void fetch(struct option *o)
 {
   *o->var.flag = !!getconf_isset(o->filename) ^ !o->value.flag;
@@ -23,11 +16,5 @@ static void fetch(struct option *o)
 const struct option_type opt_flag = {
   0,
   parse,
-  fetch
-};
-
-const struct option_type opt_counter = {
-  0,
-  counter,
   fetch
 };
