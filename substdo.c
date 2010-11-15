faab89e250072f8eefbc773cf2d431f6afe3c52e
Bruce Guenter <bruce@untroubled.org>
2010-11-15 10:21:53 -0600

    Add most -Wextra warnings, and fix nuisance warning in substdo.c

--- substdo.c
+++ substdo.c
@@ -13,7 +13,7 @@ static int allwrite(substdio_fn op,int fd,const char *buf,int len)
       if (errno == error_intr) continue;
       return -1; /* note that some data may have been written */
     }
-    if (w == 0) ; /* luser's fault */
+    if (w == 0) { } /* luser's fault */
     buf += w;
     len -= w;
   }
